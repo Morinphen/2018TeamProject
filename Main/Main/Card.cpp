1bdbfc4ddd3790848b02cef8b71dd95c459a06e7
student <student@DEV1-19>
2018-11-20 14:04:45 +0900

    表示位置変更

--- Main/Main/Card.cpp
+++ Main/Main/Card.cpp
@@ -88,9 +88,9 @@ void CObjCard::Action()
 				if (pos->WPosition[i] <= 0 && i > 1 && Summon == false)
 				{
 
-					m_x = 155 + 90 * (i - 2);
+					m_x = 498 + 90 * (i - 2);
 
-					m_y = 380;
+					m_y = 466;
 					//モンスターのパラメータ強化
 					pos->PCard[i / 2][1] += Atack;
 					pos->PCard[i / 2][2] += Guard;
@@ -126,9 +126,9 @@ void CObjCard::Action()
 				if (pos->WPosition[i] <= 0 && i > 1 && Summon == false)
 				{
 
-					m_x = 655 + 90 * (i - 4);
+					m_x = 906 + 90 * (i - 4);
 
-					m_y = 380;
+					m_y = 466;
 					pos->PCard[i / 2][1] += Atack;
 					pos->PCard[i / 2][2] += Guard;
 
@@ -297,10 +297,10 @@ void CObjCard::Action()
 					Atack = List->Action(Type,Nanber, SeedAtack);//カード番号に沿って攻撃力変動
 					Guard = List->Action(Type, Nanber, SeedGuard);//カード番号に沿って防御力変動
 
-					//右側のスペースが開いている場合
+					//左側のスペースが開いている場合
 					if (S_position == false) {
-						m_x = 200;
-						m_y = 500;
+						m_x = 543;
+						m_y = 586;
 						//Hitboxを更新し、フィールド内での処理ができるようにする
 						Hits::DeleteHitBox(this);
 						Hits::SetHitBox(this, m_x, m_y, 90, 120, ELEMENT_GREEN, OBJ_FIELD_PLAYER2, 1);
@@ -314,10 +314,10 @@ void CObjCard::Action()
 						FSummon = true;
 						Summon = true;
 					}
-					//そうでない場合、左に召喚
+					//そうでない場合、右に召喚
 					else {
-						m_x = 700;
-						m_y = 500;
+						m_x = 951;
+						m_y = 586;
 						Hits::DeleteHitBox(this);
 						Hits::SetHitBox(this, m_x, m_y, 90, 120, ELEMENT_GREEN, OBJ_FIELD_PLAYER3, 1);
 						pos->PCard[2][0] = Hp;
